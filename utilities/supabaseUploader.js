cefbe2e2599d0329084b09593e3794f96979f0be
joon1verse <hz980519@gmail.com>
2025-04-14 22:59:41 +0900

    serverless go

--- utilities/supabaseUploader.js
+++ utilities/supabaseUploader.js
@@ -33,8 +33,8 @@ import { createClient } from '@supabase/supabase-js';
 import fs from 'fs';
 
 const supabase = createClient(
-  'https://ovvxpymgnwsnyoieboxb.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im92dnhweW1nbndzbnlvaWVib3hiIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0Mzk0NDU4NCwiZXhwIjoyMDU5NTIwNTg0fQ.J0Wrm5VJYvuderrp0fKW_GTrWPISBz3IrvZYu48QjMQ'
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
 /**
