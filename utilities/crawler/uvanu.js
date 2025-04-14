2a93767c7adaa89d2d748f6c5bbfed51adbde580
joon1verse <hz980519@gmail.com>
2025-04-14 23:45:57 +0900

    serverless go

--- utilities/crawler/uvanu.js
+++ utilities/crawler/uvanu.js
@@ -72,7 +72,10 @@ function parseRelativeTime(str) {
 
 // ▶ Puppeteer로 HTML 로딩
 async function fetchHtmlWithPuppeteer(url) {
-  const browser = await puppeteer.launch({ headless: 'new' });
+  const browser = await puppeteer.launch({
+    headless: 'new', // 또는 true
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
   const page = await browser.newPage();
   await page.setUserAgent(
     'Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile Safari/604.1'
